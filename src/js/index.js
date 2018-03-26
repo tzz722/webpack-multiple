263e6c02dab4bc2961df3e822be88877f25be191
东云 <tzzdyx@qq.com>
2018-03-26 14:12:10 +0800

    up

---
+++ src/js/index.js
@@ -0,0 +1,15 @@
+'use strict';
+// 导入css
+import 'css/index';
+// 导入js
+
+document.onreadystatechange = function () {
+  if (document.readyState === 'interactive') {
+
+  }
+};
+
+
+window.onload = function () {
+
+}
